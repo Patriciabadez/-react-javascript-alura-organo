7d525b64ebffed7beb4015558a8e596c4b4bf323
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-12-23 10:15:13 -0300

    times

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,24 +4,21 @@ import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
-const Formulario = () => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e design',
-        'Mobile',
-        'Inovação e gestão'
-    ]
+const Formulario = (props) => {
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
 
     const aoSalvar = (evento) => {
-        //previne o comportamento padrão do site auto carregar 
         evento.preventDefault()
-        console.log('Form submetido', nome , cargo, imagem)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo, 
+            imagem,
+            time
+        })
+        // console.log('Form submetido', nome , cargo, imagem, time)
     }
     return (
         <section className="formulario">
@@ -31,19 +28,28 @@ const Formulario = () => {
         obrigatorio={true} 
         label="Nome" 
         placeholder="Digite seu nome"
+        valor={nome}
         aoAlterado={valor=>setNome(valor)}/>
         
         <CampoTexto obrigatorio={true} 
         label="Cargo" 
         placeholder="Digite seu cargo"
+        valor={cargo}
         aoAlterado={valor=>setCargo(valor)}
         />
         <CampoTexto 
         label="Imagem" 
         placeholder="Digite o endereço da imagem"
-        aoAlterado={valorImagem(valor)}
+        valor={imagem}
+        aoAlterado={valor=>setImagem(valor)}
         />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+        <ListaSuspensa 
+        obrigatorio={true} 
+        label="Time" 
+        itens={props.times}
+        valor={time}
+        aoAlterado={valor=>setTime(valor)}   
+        />
         <Botao>Criar Card</Botao>
     </form>
 </section>
